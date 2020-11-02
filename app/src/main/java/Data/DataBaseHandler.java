40f4a4db25de5798a9b20d8372fd46cbc0179826
4Korn <4Kornev@gmail.com>
2020-11-02 15:19:19 +0300

    Commit 1

--- app/src/main/java/Data/DataBaseHandler.java
+++ app/src/main/java/Data/DataBaseHandler.java
@@ -1,11 +1,17 @@
 package Data;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.Nullable;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import Model.Car;
 import Utils.Util;
 
 //класс для управления базой данных и наследуемся
@@ -41,5 +47,71 @@ public class DataBaseHandler extends SQLiteOpenHelper {
         //создаём новую вызывая метод
         onCreate(db);
 
+    }
+
+    //делаём возможность для функционала таблицы
+
+    //метод для добавления в таблицу
+    public void addCar(Car car) {
+        //Получаем обьект базы данных, читаем его
+        SQLiteDatabase db = this.getReadableDatabase();
+        //взаимодействуем с базой данных черз ключ значение
+        //айди добавляються автоматически
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(Util.KEY_NAME, car.getName());
+        contentValues.put(Util.KEY_PRICE, car.getPrice());
+        //вставляем в базу данных
+        db.insert(Util.TABLE_NAME, null, contentValues);
+        //акрываем соединения с базой данных
+        db.close();
+    }
+
+
+    //метод для извлечения записи по айди
+    public Car getCar(int id) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        //извлекаем записи из всех столбцов, извлекаем только по айди, и пишем нулл для других параметров сортировки
+        Cursor cursor = db.query(Util.TABLE_NAME, new String[]{Util.KEY_ID, Util.KEY_NAME, Util.KEY_PRICE},
+                Util.KEY_ID + "=?", new String[]{String.valueOf(id)},
+                null, null, null, null);
+
+        //проверяем что поиск не пустой, если ок - то запускаем
+        if (cursor != null) {
+            cursor.moveToFirst();
+        }
+        //возвращаем обьект с извлеченными данными из курсора, передаем индексы столбцов
+        Car car = new Car(Integer.parseInt(cursor.getString(0)),
+                cursor.getString(1),
+                cursor.getString(2));
+
+        return car;
+    }
+
+
+    //метод который получает все записи из таблицы
+    public List<Car> getAllCars() {
+        //считываем записи
+        SQLiteDatabase db = this.getReadableDatabase();
+        //создаём список всех записей
+        List<Car> carsList = new ArrayList<>();
+        //строка для СКЛ кода
+        String selectAllCars = "SELECT * FROM " + Util.TABLE_NAME;
+
+        Cursor cursor = db.rawQuery(selectAllCars, null);
+        //проверяем на содержания хоть каких то записей
+        if (cursor.moveToFirst()) {
+            //выполняем цикл пока цикл содержит следующий элемент создаём обьект и устанавливаем данные по колонкам
+            do {
+                Car car = new Car();
+                car.setId(Integer.parseInt(cursor.getString(0)));
+                car.setName(cursor.getString(1));
+                car.setPrice(cursor.getString(2));
+
+                //добавляем обьект с параметрами в лист
+                carsList.add(car);
+            } while (cursor.moveToNext());
+        }
+        //возвращаем заполненный лист
+        return carsList;
     }
 }
--- app/src/main/java/com/example/carssqlite/MainActivity.java
+++ app/src/main/java/com/example/carssqlite/MainActivity.java
@@ -3,6 +3,13 @@ package com.example.carssqlite;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import Data.DataBaseHandler;
+import Model.Car;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //создаём базу при запуске приложения
+        DataBaseHandler dataBaseHandler = new DataBaseHandler(this);
+
+        //добавляем записи в базу данных с параметрами из конструктора
+        dataBaseHandler.addCar(new Car("Тайота", "30 000"));
+        dataBaseHandler.addCar(new Car("Хонда", "20 000"));
+        dataBaseHandler.addCar(new Car("БМВ", "50 000"));
+        dataBaseHandler.addCar(new Car("КИЯ", "15 000"));
+
+        //создаём лист из обектов вызывая метод
+        List <Car> carList = dataBaseHandler.getAllCars();
+
+        //смотрим в лог через цикл
+        for (Car car: carList ){
+            Log.i("Car Info: ", "ID " + car.getId() + ", name "+ car.getName() + ", price " + car.getPrice());
+        }
     }
 }
